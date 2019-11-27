be64aad37fd0fac4365c71012c5d28d534b33b4f
ines <inesqtr@gmail.com>
2019-11-27 18:43:19 +0000

    created grid

--- App.js
+++ App.js
@@ -1,19 +1,75 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import Constants from './Constants';
+import Cell from './Components/Cell';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.boardWidth = Constants.CELL_SIZE * Constants.BOARD_SIZE;
+    this.grid = Array.apply(null, Array(Constants.BOARD_SIZE)).map((el, index) => {
+      return Array.apply(null, Array(Constants.BOARD_SIZE)).map((el, index) => {
+        return null;
+      });
+    });
+  }
+
+  onPressButton() {
+    alert('You tapped the button!')
+  };
+
+  renderBoard = () => {
+    return Array.apply(null, Array(Constants.BOARD_SIZE)).map((el, rowIndex) => {
+      let cellList = Array.apply(null, Array(Constants.BOARD_SIZE)).map((el, colIndex) => {
+        return <Cell key={colIndex} width={Constants.CELL_SIZE} height={Constants.CELL_SIZE} />
+      });
+
+      return (
+        <View style={{ width: this.boardWidth, height: Constants.CELL_SIZE, flexDirection: 'row' }}>
+          {cellList}
+        </View>
+      )
+    });
+  }
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>The GrinchSweeper</Text>
+        <Button
+            onPress={this.onPressButton}
+            title="Press Me"
+          />
+        <Text style={styles.description}>hedsmlksnhvkjhfkjahbscljks</Text>
+        <View style={{ width: this.boardWidth, height: this.boardWidth, backgroundColor: '#888888', flexDirection: 'column' }}>
+          {this.renderBoard()}
+        </View>
+      </View>
+    )
+
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#0D8584'
   },
+
+  title: {
+    padding: 10, 
+    fontSize: 35
+  },
+
+  description:{
+    padding: 10,
+    fontSize:15,
+    
+  }
 });
